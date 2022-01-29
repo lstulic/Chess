bda202128f0216599fb209b2b22fca0d6b4a647e
lstulic <lstulic00@gmail.com>
2022-01-29 15:49:13 +0100

    Created UserInterface class

---
+++ src/UserInterface.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class UserInterface extends Application{
+	
+	
+	
+	public void start(Stage stage) {
+		
+	}
+	
+	public static void launch() {
+		launch(UserInterface.class);
+	}
+}
