a7b38bd877b558fc921d4886bb23adab6ce43e17
lstulic <lstulic00@gmail.com>
2022-02-04 18:01:11 +0100

    added check for first move

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,9 +1,11 @@
 
 public class Pawn extends Figure{
+	private boolean moved;
 
 	public Pawn(Coordinate cords, String color, String image, String id) {
 		super(cords, color, image, id);
 		addMoves();
+		this.moved = false;
 	}
 	
 	public void addMoves() {
@@ -12,5 +14,13 @@ public class Pawn extends Figure{
 		} else {
 			setMove(1,0);
 		}
+	}
+	
+	public boolean Moved() {
+		return this.moved;
+	}
+	
+	public void Move() {
+		this.moved = true;
 	}
 }
